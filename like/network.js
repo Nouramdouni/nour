bf6a27c010a1430fd7fe11f6f2875da77e1d9ff0
unknown <nourhene803@gmail.com>
2024-10-18 12:40:17 +0100

    first commit

---
+++ like/network.js
@@ -0,0 +1,7 @@
+function likes(x) {
+   var nombre=document.querySelector(x) 
+   var som=Number(nombre.innerHTML)
+      som=som+1
+      nombre.innerHTML=som
+  }
+ 
--- protfolio.js
+++ protfolio.js
@@ -1,1 +1,54 @@
-function gamil():
+    var modal = document.getElementById("myModal");
+
+
+var btn = document.getElementById("myBtn");
+
+
+var span = document.getElementsByClassName("close")[0];
+
+ 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+// const photo = document.querySelector('.img');
+
+// setInterval(() => {
+//     photo.classList.toggle('show-border');
+// }, 3000);
+
+  document.querySelector('#cv-btn').addEventListener('click', function() {
+    console.log('CV image clicked. Download started.');
+    
+  })
+  var modal = document.getElementById("myModal");
+
+
+  var btn2 = document.getElementById("myBtn2");
+  
+  
+  var span = document.getElementsByClassName("close")[0];
+  
+   
+  btn2.onclick = function() {
+    modal.style.display = "block";
+  }
+  
+  
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
